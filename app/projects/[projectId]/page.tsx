7b1007f28b982b0f85a3eeeebf8b28def70d60f8
TheHummel <jannisalsbach@gmail.com>
2026-02-03 16:07:27 +0100

    fix: rename prop

--- app/projects/[projectId]/page.tsx
+++ app/projects/[projectId]/page.tsx
@@ -302,7 +302,7 @@ export default function ProjectPage() {
         isSaving={isSaving}
         lastSaved={lastSaved}
         hasPdfData={!!pdfData}
-        showInitializeWithAI={hasInitConfig}
+        showInitializationOption={hasInitConfig}
       />
 
       <ResizablePanelGroup
